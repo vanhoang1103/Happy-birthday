d5ebd029a516cbaa934637a3171c55e8b9dea687
vanhoang1103 <154491591+vanhoang1103@users.noreply.github.com>
2024-01-19 00:38:22 +0700

    commit

--- accset/js/index.js
+++ accset/js/index.js
@@ -26,3 +26,27 @@ modal.addEventListener('click',hideOpen)
 modalContainer.addEventListener('click', function(even){
     even.stopPropagation()
 })
+
+// Phần xem lời chúc 1
+const opens2 = document.querySelectorAll('.js-content2')
+const modal2 = document.querySelector('.js-modal2')
+const modalContainer2 = document.querySelector('.js-modal_container2')
+const modalClose2 = document.querySelector('.js-modal_icon2')
+// phần thêm class open vào modal
+function showOpen2() {
+    modal2.classList.add('open2')
+}
+// phần gỡ bỏ open ở modal
+function hideOpen2() {
+    modal2.classList.remove('open2')
+}
+// lặp qua từng thẻ button
+for(const open of opens2) {
+    open.addEventListener('click',showOpen2)
+}
+// lắng nghe hành vi onlick vào nút close
+modalClose2.addEventListener('click',hideOpen2)
+modal2.addEventListener('click',hideOpen2)
+modalContainer2.addEventListener('click', function(even){
+    even.stopPropagation()
+})
